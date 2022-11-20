e64b208565c88521e6c6b90e3c4ac726b5c5142f
Caleb Gill <calebbg@UNC-R90YDFXE.localdomain>
2022-11-20 13:25:28 -0500

    Added 404 error, reverted some functions to post

--- server.js
+++ server.js
@@ -21,16 +21,20 @@ app.get('/app/roll/', (req, res, next) => {
     res.status(200).json(roll.roll(6,2,1));
 });
 
-app.get('/app/roll/:sides', (req, res, next) => {
+app.post('/app/roll/:sides', (req, res, next) => {
     res.status(200).json(roll.roll(req.params.sides,2,1));
 });
 
-app.get('/app/roll/:sides/:dice', (req, res, next) => {
+app.post('/app/roll/:sides/:dice', (req, res, next) => {
     res.status(200).json(roll.roll(req.params.sides,req.params.dice,1));
 });
 
-app.get('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
+app.post('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
     res.status(200).json(roll.roll(req.params.sides,req.params.dice,req.params.rolls));
+});
+
+app.get('*', (req, res, next) => {
+    res.status(404);
 });
 
 app.listen(port, () => {
