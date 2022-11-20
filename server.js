4aa1285be657b7d05c274f7a2f471f10314a69a2
Caleb Gill <calebbg@UNC-R90YDFXE.localdomain>
2022-11-20 11:33:21 -0500

    Included imports and basic temp endpoints

--- server.js
+++ server.js
@@ -2,6 +2,22 @@
 
 const minimist = require('minimist');
 const roll = require('./lib/roll.js');
+const express = require('express');
+const app = express();
 
-//const args = minimist(process.argv.slice(2));
-console.log(roll.roll(6,3,3));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const args = minimist(process.argv.slice(2));
+
+const port = args.port || 5000;
+
+app.get('/', (req, res, next) => {
+    res.send("Hello,world");
+})
+
+app.listen(port, () => {
+    console.log("Server is listening on port " + port);
+});
+
+//console.log(roll.roll(6,3,3));
