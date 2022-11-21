dec917d634b95d248fc07709c2c8abd5a95f371c
Caleb Gill <calebbg@UNC-R90YDFXE.localdomain>
2022-11-20 23:48:31 -0500

    Added .end() to endpoints

--- server.js
+++ server.js
@@ -14,23 +14,23 @@ const port = args.port || 5000;
 
 app.get('/app/', (req, res, next) => {
     res.status(200);
-    res.send("Status code : 200 OK")
+    res.send("Status code : 200 OK");
 });
 
 app.post('/app/roll/', (req, res, next) => {
-    res.status(200).json(roll.roll(6,2,1));
+    res.status(200).json(roll.roll(6,2,1)).end();
 });
 
 app.post('/app/roll/:sides', (req, res, next) => {
-    res.status(200).json(roll.roll(req.params.sides,2,1));
+    res.status(200).json(roll.roll(req.params.sides,2,1)).end();
 });
 
 app.post('/app/roll/:sides/:dice', (req, res, next) => {
-    res.status(200).json(roll.roll(req.params.sides,req.params.dice,1));
+    res.status(200).json(roll.roll(req.params.sides,req.params.dice,1)).end();
 });
 
 app.post('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
-    res.status(200).json(roll.roll(req.params.sides,req.params.dice,req.params.rolls));
+    res.status(200).json(roll.roll(req.params.sides,req.params.dice,req.params.rolls)).end();
 });
 
 // app.get('*', (req, res, next) => {
@@ -38,5 +38,5 @@ app.post('/app/roll/:sides/:dice/:rolls', (req, res, next) => {
 // });
 
 app.listen(port, () => {
-    //console.log("Server is listening on port " + port);
+    console.log("Server is listening on port " + port);
 });
